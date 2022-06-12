28a349832c91db0a3ff4f09b550d9538d8255c67
AmosJune <amos.sifuma@student.moringaschool.com.com>
2022-06-12 16:40:32 +0300

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,5 @@
 // Code your solution here
 
-// const drivers = [
-//     {
-//       name: 'Bobby',
-//       hometown: 'Pittsburgh' },
-//     {
-//       name: 'Sammy',
-//       hometown: 'New York' } ,
-//     {
-//       name: 'Sally',
-//       hometown: 'Cleveland' },
-//     {
-//       name: 'Annette',
-//       hometown: 'Los Angeles' },
-//     {
-//       name: 'Bobby',
-//       hometown: 'Tampa Bay' }
-//   ];
-
 function findMatching(string, drivers){
     return string.filter(
         result => result.toLowerCase() === drivers.toLowerCase()
