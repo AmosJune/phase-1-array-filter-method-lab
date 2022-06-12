f48e3f521c8755c02fcaa94f2fcc0efb2b017389
AmosJune <amos.sifuma@student.moringaschool.com.com>
2022-06-12 16:36:02 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution here
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function findMatching(string, drivers){
+    return string.filter(
+        result => result.toLowerCase() === drivers.toLowerCase()
+    )
+};
+
+function fuzzyMatch(string, drivers){
+    return string.filter(result => result[0] === drivers[0]);
+};
+
+function matchName(string, drivers){
+    return string.filter(record => record.name === drivers)
+};
+
